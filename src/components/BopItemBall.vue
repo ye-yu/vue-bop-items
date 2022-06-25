7d79a92ef3a89b2b5202c5a97f2a0755f0dcbb2b
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2022-06-25 22:34:35 +0800

    Play sound on start

--- src/components/BopItemBall.vue
+++ src/components/BopItemBall.vue
@@ -6,7 +6,8 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { Note, playSineWave } from "@/utils/sound.util";
+import { defineComponent, type PropType } from "vue";
 
 export default defineComponent({
   props: {
@@ -18,6 +19,30 @@ export default defineComponent({
     movementDuration: {
       type: [Number, String],
       default: 2,
+      validate: (n: string | number) => !isNaN(Number(n)),
+    },
+    playSound: {
+      type: Number as PropType<Note>,
+      default: Note.A4,
+    },
+  },
+  data(): {
+    timeoutId: number | null,
+  } {
+    return {
+      timeoutId: null,
+    }
+  },
+  methods: {
+    startSoundLoop() {
+      playSineWave(this.playSound);
+      this.timeoutId = setTimeout(() => {
+        this.startSoundLoop();
+      }, Number(this.movementDuration) * 500)
+    },
+    stopSoundLoop() {
+      if (typeof this.timeoutId !== "number") return
+      clearTimeout(this.timeoutId);
     }
   },
   computed: {
@@ -27,6 +52,13 @@ export default defineComponent({
     movementDurationSecond() {
       return `${this.movementDuration}s`
     }
+  },
+  updated() {
+    if (this.start) this.startSoundLoop()
+    else this.stopSoundLoop()
+  },
+  unmounted() {
+    this.stopSoundLoop()
   }
 })
 </script>
