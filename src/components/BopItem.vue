8a35c2158817a2e988046f089e1c01c30669a1e8
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2022-06-25 23:13:18 +0800

    Change color order

--- src/components/BopItem.vue
+++ src/components/BopItem.vue
@@ -5,13 +5,13 @@ import { Note } from "@/utils/sound.util";
 </script>
 
 <template>
-  <BopItemBallVue :play-sound="Note.C4" movement-duration="3" color="red" :start="start" />
-  <BopItemBallVue :play-sound="Note.E4" movement-duration="3.5" color="orange" :start="start" />
-  <BopItemBallVue :play-sound="Note.G4" movement-duration="4" color="yellow" :start="start" />
-  <BopItemBallVue :play-sound="Note.B4" movement-duration="4.5" color="green" :start="start" />
-  <BopItemBallVue :play-sound="Note.D5" movement-duration="5" color="blue" :start="start" />
-  <BopItemBallVue :play-sound="Note.F5" movement-duration="5.5" color="purple" :start="start" />
-  <BopItemBallVue :play-sound="Note.A5" movement-duration="6" color="indigo" :start="start" />
+  <BopItemBallVue :play-sound="Note.C4" movement-duration="1.4" color="red" :start="start" />
+  <BopItemBallVue :play-sound="Note.E4" movement-duration="1.5" color="orange" :start="start" />
+  <BopItemBallVue :play-sound="Note.G4" movement-duration="1.6" color="yellow" :start="start" />
+  <BopItemBallVue :play-sound="Note.B4" movement-duration="1.7" color="green" :start="start" />
+  <BopItemBallVue :play-sound="Note.D5" movement-duration="1.8" color="blue" :start="start" />
+  <BopItemBallVue :play-sound="Note.F5" movement-duration="1.9" color="indigo" :start="start" />
+  <BopItemBallVue :play-sound="Note.A5" movement-duration="2.0" color="purple" :start="start" />
 </template>
 
 <script lang="ts">
