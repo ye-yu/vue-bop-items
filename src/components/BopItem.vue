dc4dd9b17f58fa585be4f1e989932e4ad3acedcd
Ye-Yu <30603031+ye-yu@users.noreply.github.com>
2022-06-25 22:37:14 +0800

    Play different sound for each ball

--- src/components/BopItem.vue
+++ src/components/BopItem.vue
@@ -1,16 +1,17 @@
 <script setup lang="ts">
 import { defineComponent } from "vue";
 import BopItemBallVue from "./BopItemBall.vue";
+import { Note } from "@/utils/sound.util";
 </script>
 
 <template>
-  <BopItemBallVue movement-duration="3" color="red" :start="start" />
-  <BopItemBallVue movement-duration="3.5" color="orange" :start="start" />
-  <BopItemBallVue movement-duration="4" color="yellow" :start="start" />
-  <BopItemBallVue movement-duration="4.5" color="green" :start="start" />
-  <BopItemBallVue movement-duration="5" color="blue" :start="start" />
-  <BopItemBallVue movement-duration="5.5" color="purple" :start="start" />
-  <BopItemBallVue movement-duration="6" color="indigo" :start="start" />
+  <BopItemBallVue :play-sound="Note.C4" movement-duration="3" color="red" :start="start" />
+  <BopItemBallVue :play-sound="Note.E4" movement-duration="3.5" color="orange" :start="start" />
+  <BopItemBallVue :play-sound="Note.G4" movement-duration="4" color="yellow" :start="start" />
+  <BopItemBallVue :play-sound="Note.B4" movement-duration="4.5" color="green" :start="start" />
+  <BopItemBallVue :play-sound="Note.D5" movement-duration="5" color="blue" :start="start" />
+  <BopItemBallVue :play-sound="Note.F5" movement-duration="5.5" color="purple" :start="start" />
+  <BopItemBallVue :play-sound="Note.A5" movement-duration="6" color="indigo" :start="start" />
 </template>
 
 <script lang="ts">
